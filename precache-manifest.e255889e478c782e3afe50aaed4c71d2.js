bed4450597ca01271ef7b5efd1a9c2078124ec18
y0092888 <ju-horn@web.de>
2020-07-29 14:51:22 +0200

    Updates

---
+++ precache-manifest.e255889e478c782e3afe50aaed4c71d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d651005c1a6da72b368379ea3f02b54",
+    "url": "/memeCreator/index.html"
+  },
+  {
+    "revision": "e5dfdbe0ff09af18fdeb",
+    "url": "/memeCreator/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c2ca96661f5fc75c6e5e",
+    "url": "/memeCreator/static/js/2.a7102cb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memeCreator/static/js/2.a7102cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5dfdbe0ff09af18fdeb",
+    "url": "/memeCreator/static/js/main.4521405f.chunk.js"
+  },
+  {
+    "revision": "9090b8161ee4797d4155",
+    "url": "/memeCreator/static/js/runtime-main.3c140523.js"
+  }
+]);
