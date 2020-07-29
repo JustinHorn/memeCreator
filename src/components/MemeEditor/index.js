cce8a6eea235feef4b3e006f1581cc70da6adc73
Bruno DeLucca <bruno@delucca.cl>
2020-07-29 17:54:52 +0200

    second layout

--- src/components/MemeEditor/index.js
+++ src/components/MemeEditor/index.js
@@ -2,10 +2,30 @@ import React from "react";
 
 import { useParams } from "react-router-dom";
 
+import domtoimage from "dom-to-image";
+
 import styles from "./index.module.css";
+
+import anrgyImg from "../../images/AngrySection.jpeg";
 
 export default () => {
   let { memetype } = useParams();
+
+  setTimeout(() => {
+    const node = document.querySelector("." + styles.meme);
+
+    console.log(node);
+
+    domtoimage
+      .toPng(node)
+      .then(function (dataUrl) {
+        const img = document.querySelector("." + styles.resultImage);
+        img.src = dataUrl;
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error);
+      });
+  }, 1000);
 
   return (
     <div>
@@ -15,19 +35,33 @@ export default () => {
           <h3>This is the MemeEditor </h3>
           <div>
             page with memetype:
-            <b>
-              <i> {memetype} </i>
-            </b>
+            <i> {memetype} </i>
           </div>
         </div>
-        <div className={styles.options}>
-          <input type="text" value="Option 1" />
-          <input type="text" value="Option 2" />
-          <input type="text" value="Option 3" />
+        <div className={styles.bodyContainer}>
+          <div className={styles.meme}>
+            {/* Meme goes here*/}
+            <div>meme text top</div>
+            <div
+              className={styles.memeImage}
+              style={{ backgroundImage: "url(" + anrgyImg + ")" }}
+            >
+              Image
+            </div>
+            <div className={styles.textBottom}>meme text bottom</div>
+          </div>
+          <div className={styles.options}>
+            <input type="text" value="Option 1" />
+            <input type="text" value="Option 2" />
+            <input type="text" value="Option 3" />
+          </div>
         </div>
-        <div className={styles.meme}>{/* Meme goes here*/}</div>
       </div>
-      <img src="" alt="this will be the result" />
+      <img
+        src=""
+        className={styles.resultImage}
+        alt="this will be the result"
+      />
     </div>
   );
 };
