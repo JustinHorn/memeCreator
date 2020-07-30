10e955ae376c85534dc081bd97c3915c057cae51
Bruno DeLucca <bruno@delucca.cl>
2020-07-30 17:40:37 +0200

    Updates

---
+++ precache-manifest.d780ff5410ce306c7c403086d9d70330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122a1db8b682f764015aaf51cae3ca06",
+    "url": "/memeCreator/index.html"
+  },
+  {
+    "revision": "cbb60d350c5ae6461583",
+    "url": "/memeCreator/static/css/main.f4501c55.chunk.css"
+  },
+  {
+    "revision": "45747db75911e6f21dad",
+    "url": "/memeCreator/static/js/2.65794a0f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memeCreator/static/js/2.65794a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb60d350c5ae6461583",
+    "url": "/memeCreator/static/js/main.b8f2e0bd.chunk.js"
+  },
+  {
+    "revision": "9090b8161ee4797d4155",
+    "url": "/memeCreator/static/js/runtime-main.3c140523.js"
+  }
+]);
