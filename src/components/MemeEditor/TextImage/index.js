82cb6ecb2022e7df2e62856a76fea6e762d88482
y0092888 <ju-horn@web.de>
2020-07-30 15:47:29 +0200

    refactor 50% Co-authored-by: BDeLucca <bruno@delucca.cl>

---
+++ src/components/MemeEditor/TextImage/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+import styles from "./index.module.css";
+
+import TextInImage from "./text_image/In";
+import TextOuOfImage from "./text_image/Out";
+
+export default ({ downloadImg, image, getImage, textIn }) => {
+  const [topText, setTopText] = useState("top text");
+  const [bottomText, setBottomText] = useState("bottom text");
+
+  return (
+    <div className={styles.bodyContainer}>
+      {(textIn && (
+        <TextInImage image={image} topText={topText} bottomText={bottomText} />
+      )) || (
+        <TextOuOfImage
+          image={image}
+          topText={topText}
+          bottomText={bottomText}
+        />
+      )}
+
+      <div className={styles.options}>
+        <input
+          type="text"
+          value={topText}
+          onChange={(e) => {
+            setTopText(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          value={bottomText}
+          onChange={(e) => {
+            setBottomText(e.target.value);
+          }}
+        />
+        <button onClick={() => downloadImg(styles.meme)}>Download Image</button>
+        <input
+          type="file"
+          name="load image"
+          accept="image/png, image/jpeg"
+          onChange={getImage}
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/MemeEditor/index.js
+++ src/components/MemeEditor/index.js
@@ -6,20 +6,76 @@ import domeToImage from "dom-to-image";
 
 import styles from "./index.module.css";
 
-import TextOutOfImage from "./textOutImage";
-import TextInImage from "./textInImage";
+import TextImage from "./TextImage";
+
+import anrgyImg from "../../images/AngrySection.jpeg";
 
 export default () => {
   const { memeType } = useParams();
   // textInOfImage or textOutOfImage
+
+  const img = new Image();
+  img.src = anrgyImg;
+
+  const [image, setImage] = useState(img);
+
+  const getImage = (e) => {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const img = new Image();
+
+      img.src = e.target.result;
+      setTimeout(() => {
+        const MAX_SIZE = 500;
+        if (img.height > MAX_SIZE || img.width > MAX_SIZE) {
+          const ratio = img.height / img.width;
+
+          if (img.height > img.width) {
+            img.height = MAX_SIZE;
+            img.width = MAX_SIZE / ratio;
+          } else {
+            img.width = MAX_SIZE;
+            img.height = MAX_SIZE * ratio;
+          }
+        }
+        setTimeout(() => {
+          setImage(img);
+        }, 0);
+      }, 0);
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
     <div className={styles.editor}>
       <div className={styles.header}>
         <h3>This is the MemeEditor </h3>
       </div>
-      {(memeType === "textInOfImage" && <TextInImage></TextInImage>) || (
-        <TextOutOfImage></TextOutOfImage>
-      )}
+      <TextImage
+        textIn={memeType === "textInOfImage"}
+        downloadImg={downloadImg}
+        image={image}
+        getImage={getImage}
+      />
     </div>
   );
 };
+
+function downloadImg(className) {
+  const node = document.querySelector("." + className);
+  domeToImage
+    .toPng(node)
+    .then(function (dataUrl) {
+      downloadMeme(dataUrl);
+    })
+    .catch(function (error) {
+      console.error("oops, something went wrong!", error);
+    });
+}
+
+function downloadMeme(dataUrl) {
+  var link = document.createElement("a");
+  link.download = "meme.jpg";
+  link.href = dataUrl;
+  link.click();
+}
