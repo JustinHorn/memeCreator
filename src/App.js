eff71671c946754f8b04f4055a1ddbd3b15dc088
y0092888 <ju-horn@web.de>
2020-08-11 08:27:04 +0200

    lessRouting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import IntroPage from "./components/IntroPage";
 import MemeEditor from "./components/MemeEditor";
-
-import MemeTypes from "./components/MemeTypes";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/memeCreator/memetypes/:memeType">
+        <Route path="/memeCreator/:memeType">
           <MemeEditor />
-        </Route>
-        <Route path="/memeCreator/memetypes">
-          <MemeTypes />
         </Route>
         <Route path="/memeCreator">
           <IntroPage />
         </Route>
-        <Route path="/"></Route>
+        <Route path="*">
+          <Redirect to="/memeCreator"></Redirect>
+        </Route>
       </Switch>
     </Router>
   );
--- src/components/IntroPage/index.js
+++ src/components/IntroPage/index.js
@@ -24,9 +24,24 @@ export default () => {
       <br />
       <h1>Welcome to the Meme creator</h1>
       <br />
-      <Link to="/memeCreator/memetypes" className={styles.Link}>
-        Get Started
-      </Link>
+      <div>
+        <Link className={styles.linkMemeTypes} to="/memeCreator/textInOfImage">
+          Text in image
+          <img
+            className={styles.memeTypesImg}
+            src="https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk"
+            alt="funny"
+          ></img>
+        </Link>
+        <Link className={styles.linkMemeTypes} to="/memeCreator/freestyle">
+          <h3>Editor 2</h3>
+          <img
+            className={styles.memeTypesImg}
+            src="https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk"
+            alt="serious"
+          ></img>
+        </Link>
+      </div>
       <br />
       <div className={styles.imgContainer}>
         {meme.slice(0, 20).map((x) => (
